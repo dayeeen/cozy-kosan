647d5cde3ac21ca11fb5c992206183c2970c1f94
dayeeen <nubicodes@gmail.com>
2025-04-22 07:57:17 +0700

    add BottomNavbarItem widget for navigation in HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:bwa_cozy/models/space.dart';
 import 'package:bwa_cozy/models/tips.dart';
 import 'package:bwa_cozy/theme.dart';
+import 'package:bwa_cozy/widgets/bottom_navbar_item.dart';
 import 'package:bwa_cozy/widgets/city_card.dart';
 import 'package:bwa_cozy/widgets/space_card.dart';
 import 'package:bwa_cozy/widgets/tips_card.dart';
@@ -17,9 +18,7 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         bottom: false,
         child: Padding(
-          padding: EdgeInsets.all(
-             edge,
-          ),
+          padding: EdgeInsets.all(edge),
           child: ListView(
             children: [
               // Header
@@ -31,16 +30,14 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 2),
-              Text('Mencari kosan yang cozy',
-                style: greyTextStyle.copyWith(
-                  fontSize: 16,
-                ),
+              Text(
+                'Mencari kosan yang cozy',
+                style: greyTextStyle.copyWith(fontSize: 16),
               ),
               SizedBox(height: 30),
-              Text('Popular Cities',
-                style: regularTextStyle.copyWith(
-                  fontSize: 16,
-                )
+              Text(
+                'Popular Cities',
+                style: regularTextStyle.copyWith(fontSize: 16),
               ),
               SizedBox(height: 16),
               // List of Cities
@@ -62,7 +59,7 @@ class HomePage extends StatelessWidget {
                         id: 2,
                         name: 'Bandung',
                         imageUrl: 'assets/city2.png',
-                        isPopular: true
+                        isPopular: true,
                       ),
                     ),
                     SizedBox(width: 20),
@@ -78,10 +75,9 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 30),
-              Text('Recommended Space',
-                style: regularTextStyle.copyWith(
-                  fontSize: 16,
-                )
+              Text(
+                'Recommended Space',
+                style: regularTextStyle.copyWith(fontSize: 16),
               ),
               SizedBox(height: 16),
               // List of Space
@@ -126,10 +122,9 @@ class HomePage extends StatelessWidget {
               ),
               SizedBox(height: 30),
               // TIPS & GUIDANCE
-              Text('Tips & Guidance',
-                style: regularTextStyle.copyWith(
-                  fontSize: 16,
-                )
+              Text(
+                'Tips & Guidance',
+                style: regularTextStyle.copyWith(fontSize: 16),
               ),
               SizedBox(height: 16),
               Column(
@@ -154,11 +149,33 @@ class HomePage extends StatelessWidget {
                   ),
                   SizedBox(height: 20),
                 ],
-              )
+              ),
             ],
           ),
         ),
       ),
+      floatingActionButton: Container(
+        width: MediaQuery.of(context).size.width - (2 * edge),
+        height: 65,
+        margin: EdgeInsets.symmetric(horizontal: edge),
+        decoration: BoxDecoration(
+          color: Color(0xffF6F7F8),
+          borderRadius: BorderRadius.circular(23),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            BottomNavbarItem(imageUrl: 'assets/icon_home.png', isActive: true),
+            BottomNavbarItem(
+              imageUrl: 'assets/icon_email.png',
+              isActive: false,
+            ),
+            BottomNavbarItem(imageUrl: 'assets/icon_card.png', isActive: false),
+            BottomNavbarItem(imageUrl: 'assets/icon_love.png', isActive: false),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
